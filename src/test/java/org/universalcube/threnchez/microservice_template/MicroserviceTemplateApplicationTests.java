489fbed0eacdc727a4d4630b050c7f005565b692
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2024-12-30 16:57:32 +0100

    feat(template): initialize microservice template
    ---
    Add base Spring Boot application with entry point and context load test.

---
+++ src/test/java/org/universalcube/threnchez/microservice_template/MicroserviceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.universalcube.threnchez.microservice_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
